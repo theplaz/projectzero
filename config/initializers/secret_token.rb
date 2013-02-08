4b8ff6d9d6ab897ea5c600bea56c41241416113f
Michael Plasmeier <plaz@theplaz.com>
2013-02-08 02:27:52 -0500

    template for P0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectzero::Application.config.secret_token = '82804e813c0a59bfc026bf32163b4304097e2bfdd943a2541cee0efe8e5de0cc113011ae81dfe49c57b86266c2763aa73379218ce402d9a96a55814f93c24b13'
